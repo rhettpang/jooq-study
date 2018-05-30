bfc038446691d4519c625c60b75ffe3902d37a07
pangkunkun <pangkunkun@sightp.com>
2018-05-30 22:59:45 +0800

    jooq

---
+++ src/main/java/com/pk/JooqStudyApplication.java
@@ -0,0 +1,14 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Created by pangkunkun on 2018/5/30.
+ */
+@SpringBootApplication
+public class JooqStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JooqStudyApplication.class,args);
+    }
+}
